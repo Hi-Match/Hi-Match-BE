1e44532661ecf26b9d1867b38ba7c4eda4af4b68
eunah0507 <eunah0507@naver.com>
2025-04-03 21:59:48 +0900

    :sparkles:feat: HiMatch 잘부탁드립니다 초기 설정 push

---
+++ src/main/java/kr/co/himatch/thanksyouplz/ThanksyouplzApplication.java
@@ -0,0 +1,13 @@
+package kr.co.himatch.thanksyouplz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThanksyouplzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThanksyouplzApplication.class, args);
+	}
+
+}
