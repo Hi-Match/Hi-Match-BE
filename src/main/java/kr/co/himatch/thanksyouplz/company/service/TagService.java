cf4aedec1eb3d79748215bbf681f14be6881c253
DESKTOP-LJQ78DH\의석 <dkdlelxoa@naver.com>
2025-05-06 00:52:23 +0900

    ✨feat: 태그 서비스 주석 추가

--- src/main/java/kr/co/himatch/thanksyouplz/company/service/TagService.java
+++ src/main/java/kr/co/himatch/thanksyouplz/company/service/TagService.java
@@ -5,5 +5,6 @@ import kr.co.himatch.thanksyouplz.company.dto.CompanyInfoTagResponseDTO;
 import java.util.List;
 
 public interface TagService {
+    // 모든 태그 조회
     List<CompanyInfoTagResponseDTO> tagList();
 }
